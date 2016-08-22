01c06e36727403f5ad3ecc01eaa797d3e9edc71b
Danil Pismenny <danil@brandymint.ru>
2016-08-22 22:03:21 +0300

    Improve Client::Errors

--- lib/amo_crm/client/errors.rb
+++ lib/amo_crm/client/errors.rb
@@ -3,13 +3,12 @@ require_relative 'error'
 class AmoCRM::Client
   class Errors
     def self.build res
-
       if res.try(:body).present?
-        body = JSON.parse(res.body)
-        AmoCRM.logger.error "AmoCRM::Client: #{res.class} #{res.status}: #{res.env.url.to_s}\n#{body}"
+        AmoCRM.logger.error "AmoCRM::Client: #{res.class} #{res.headers['Content-Type']} #{res.status}: #{res.env.url.to_s}\n#{body}"
+        body = JSON.parse(res.body) rescue { 'response' => body }
         raise Error.new res.status, body['response']
       else
-        AmoCRM.logger.error "AmoCRM::Client: #{res}"
+        AmoCRM.logger.error "AmoCRM::Client: #{res.class} #{res}"
         raise Error.new 999, res.to_json
       end
     end
