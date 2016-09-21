0395df42d9782b80594acc514412b1796ac79d2e
Danil Pismenny <danil@brandymint.ru>
2016-09-21 16:51:23 +0300

    check authorization in every connection

--- lib/amo_crm/client.rb
+++ lib/amo_crm/client.rb
@@ -7,6 +7,8 @@ require_relative 'client/errors'
 class AmoCRM::Client
   PREFIX_PATH = '/private/api/v2/json/'
 
+  SESSION_TIMEOUT = 15*60 # 15 minutes - https://developers.amocrm.ru/rest_api/auth.php
+
   def initialize url:
     @connection = Faraday.new url
     # 'private/api/v2/json/'
@@ -19,8 +21,18 @@ class AmoCRM::Client
     logger.debug "Client: Authorize #{user_login}"
     path = "private/api/auth.php?type=json"
     data = { USER_LOGIN: user_login, USER_HASH: user_hash }
+    @user_login = user_login
+    @user_hash = user_hash
     response = connection.post path, data
+    # Success reponse header example:
+    # "set-cookie"=> "session_id=nic2a7t2mauuooc8a45pbm3p85nlf1nm9tr4cltpfl3r5kfk4um0; path=/; domain=.amocrm.ru, BITRIX_SM_LOGIN=danil%40brandymint.ru; expires=Thu, 26-Aug-2021 13:34:47 GMT; Max-
+    # "expires"=>"Thu, 19 Nov 1981 08:52:00 GMT",
+    #
+    # Success response body example:
+    # "{\"response\":{\"auth\":true,\"accounts\":[{\"id\":\"11413681\",\"name\":\"Kiosk\",\"subdomain\":\"kiosk\",\"language\":\"ru\",\"timezone\":\"Europe\\/Moscow\"}],\"server_time\":1474464887}}"
+    @authorized_at = Time.zone.now
     @cookie = response.headers['set-cookie']
+    logger.debug "Client: Authorization response #{response.body}"
     validate response
   end
 
@@ -33,28 +45,45 @@ class AmoCRM::Client
 
   def get path, params={}
     logger.debug "Client: GET #{path} #{params}"
+    check_authorization!
     validate connection.get PREFIX_PATH + path, params, { Cookie: cookie }
   end
 
   def post path, data=nil
     data = JSON.generate data.as_json
     logger.debug "Client: POST #{path}: #{data}"
+    check_authorization!
     validate connection.post PREFIX_PATH + path, data, { 'Content-Type' => 'application/json', Cookie: cookie }
   end
 
   def put path, data
     data = JSON.generate data.as_json
     logger.debug "Client: PUT #{path}: #{data}"
+    check_authorization!
     validate connection.put PREFIX_PATH + path, data, { 'Content-Type' => 'application/json', Cookie: cookie }
   end
 
   def delete path
+    check_authorization!
     validate connection.delete PREFIX_PATH path
   end
 
   private
 
   attr_reader :connection, :cookie
+
+  def check_authorization!
+    return true unless session_expired?
+    raise 'No user_login to reauthorize' unless @user_login
+    res = authorize user_login: @user_login, user_hash: @user_hash
+    raise "AmoCRM is not reauthorized #{res}" unless res.auth
+
+    res
+  end
+
+  def session_expired?
+    return !cookie || !@authorized_at || @authorized_at + SESSION_TIMEOUT <= Time.zone.now
+  end
 
   def logger
     AmoCRM.logger
